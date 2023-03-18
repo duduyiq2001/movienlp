86ec957df3ec4abb6d6373d1844297a0f01b4f74
duduyiq2001 <86567562+duduyiq2001@users.noreply.github.com>
2023-03-17 21:15:36 -0700

    buildlog

--- code/log_reg_scikit.py
+++ code/log_reg_scikit.py
@@ -3,10 +3,10 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import zero_one_loss as J01
 from joblib import dump, load
 import tensorflow as tf
-inputdata = tf.io.parse_tensor(tf.io.read_file("mixedinputbow.txt"),out_type=tf.int32)
-inputlabel = tf.io.parse_tensor(tf.io.read_file("inputlabelbow.txt"),out_type=tf.int32)
-Xte = tf.io.parse_tensor(tf.io.read_file("mixedinputbowtest.txt"),out_type=tf.int32)
-Yte = tf.io.parse_tensor(tf.io.read_file("inputlabelbowtest.txt"),out_type=tf.int32)
+inputdata = tf.io.parse_tensor(tf.io.read_file("mixedinput1.txt"),out_type=tf.float32)
+inputlabel = tf.io.parse_tensor(tf.io.read_file("inputlabel1.txt"),out_type=tf.float32)
+Xte = tf.io.parse_tensor(tf.io.read_file("mixedinputtest.txt"),out_type=tf.float32)
+Yte = tf.io.parse_tensor(tf.io.read_file("inputlabeltest.txt"),out_type=tf.float32)
 inputlabel1 = [1 if a == 1 else 0 for a in inputlabel]
 inputlabel1 = tf.convert_to_tensor(inputlabel1, dtype=tf.int32)
 Yte= [1 if a == 1 else 0 for a in Yte]
