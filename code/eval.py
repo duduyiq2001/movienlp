e63d042d220852a622399b2be36c128f78efe653
duduyiq2001 <86567562+duduyiq2001@users.noreply.github.com>
2023-03-18 20:03:03 -0700

    new model

--- code/eval.py
+++ code/eval.py
@@ -2,10 +2,10 @@ import tensorflow as tf
 from sklearn.metrics import zero_one_loss
 physical_devices = tf.config.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
-new_model = tf.keras.models.load_model('NN_longresnetwwithtworesblocksSB')
+new_model = tf.keras.models.load_model('NN_longresnetwwithtworesblocksBOW')
 
-testdata = tf.io.parse_tensor(tf.io.read_file("mixedinputtest.txt"),out_type=tf.float32)
-testlabel = tf.io.parse_tensor(tf.io.read_file("inputlabeltest.txt"),out_type=tf.float32)
+testdata = tf.io.parse_tensor(tf.io.read_file("mixedinputbowtest.txt"),out_type=tf.int32)
+testlabel = tf.io.parse_tensor(tf.io.read_file("inputlabelbowtest.txt"),out_type=tf.int32)
 testlabel1 = [1 if a == 1 else 0 for a in testlabel]
 print(len(testlabel1))
 testlabel1 = tf.convert_to_tensor(testlabel1, dtype=tf.int32)
--- code/longresnetmoded.py
+++ code/longresnetmoded.py
@@ -11,17 +11,17 @@ from tensorflow.keras.layers import Add
 from tensorflow.keras.layers import Concatenate
 
 #contruct model
-featureinput = Input(shape = (384,))
+featureinput = Input(shape = (500,))
 #resblock one
-input2 = Dense(384,activation = 'relu')(featureinput)
+input2 = Dense(500,activation = 'relu')(featureinput)
 input25 = Dropout(rate = 0.2)(input2)
-inputres2 = Add()([featureinput,input25])
+inputres2 = Concatenate()([featureinput,input25])
 #resblock two
-input3 = Dense(384,activation = 'relu')(inputres2)
-input4 = Dense(384,activation = 'relu')(input3)
-inputres3 = Add()([inputres2,input4])
+input3 = Dense(400,activation = 'relu')(inputres2)
+input4 = Dense(300,activation = 'relu')(input3)
+inputres3 = Concatenate()([inputres2,input4])
 #resblock three
-input5 = Dense(300,activation = 'relu')(inputres3)
+input5 = Dense(200,activation = 'relu')(inputres3)
 input6 = Dense(100,activation = 'relu')(input5)
 
 input7 = Concatenate()([inputres3,input6])
@@ -40,8 +40,8 @@ out = tf.keras.layers.Dense(4)(added)
 model = tf.keras.models.Model(inputs=[input1, input2], outputs=out) in the second last line, is it passing the output of the add as the input of the dense layer?
 '''
 
-inputdata = tf.io.parse_tensor(tf.io.read_file("mixedinput1.txt"),out_type=tf.float32)
-inputlabel = tf.io.parse_tensor(tf.io.read_file("inputlabel1.txt"),out_type=tf.float32)
+inputdata = tf.io.parse_tensor(tf.io.read_file("mixedinputbow.txt"),out_type=tf.int32)
+inputlabel = tf.io.parse_tensor(tf.io.read_file("inputlabelbow.txt"),out_type=tf.int32)
 inputlabel1 = [1 if a == 1 else 0 for a in inputlabel]
 inputlabel1 = tf.convert_to_tensor(inputlabel1, dtype=tf.int32)
 train_size = int(0.8*len(inputdata))
@@ -65,4 +65,4 @@ model.summary()
 for i, loss in enumerate(model.history.history['loss']):
     print(f"Epoch {i}: Training loss = {loss}")
 
-model.save("NN_longresnetwwithtworesblocksSB")
+model.save("NN_longresnetwwithtworesblocksBOW")
