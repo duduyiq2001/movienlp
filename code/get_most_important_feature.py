1ffe34c3b9d3d8f8ca26c5bf9dc5909649f1a132
duduyiq2001 <86567562+duduyiq2001@users.noreply.github.com>
2023-03-22 22:17:10 -0700

    update1

---
+++ code/get_most_important_feature.py
@@ -0,0 +1,18 @@
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+import tensorflow as tf
+with open('vectorizer.pkl', 'rb') as file:
+    vectorizer = pickle.load(file)
+alist = list(vectorizer.vocabulary_.keys())
+mostimportant = [257, 94, 432, 177, 414, 346, 224, 43, 221, 434, 27, 243, 11, 290, 417, 258, 59, 24, 361, 369, 244, 52, 254, 356, 198, 398, 431, 57, 34, 311, 205, 307, 90, 166, 350, 228, 92, 120, 136, 499, 77, 292, 201, 299, 65, 481, 151, 112, 111, 46]
+print([alist[k] for k in mostimportant])
+data_str = """
+data:0.0
+...
+data:0.99
+"""
+
+data_list = data_str.split('\n')[1:-1]  # Split the string by lines and remove the first and last empty lines
+numbers = [float(line.split(':')[1]) for line in data_list]  # Extract the number from each line and convert to float
+
+print(numbers)
